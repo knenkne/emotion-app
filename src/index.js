272a6803bbf50187af618d59c3d80669e7a43f3a
knenkne <jztenk@gmail.com>
2021-01-11 23:21:20 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import styled from "@emotion/styled"
+import css from "@emotion/css"
+
+
+const getEmotion11Library1Chunk = async () => {
+  return await import("../../emotion-11-library/build/index")
+}
+
+const getEmotion11Library2Chunk = async () => {
+  return await import("../../emotion-11-library-2/build/index")
+}
+
+setTimeout(() => {
+  getEmotion11Library1Chunk().then((chunk) => {
+    ReactDOM.render(
+      <chunk.Library1 />,
+      document.getElementById(`placeholder1`)
+    )
+  })
+}, 2000)
+
+setTimeout(() => {
+  getEmotion11Library2Chunk().then((chunk) => {
+    ReactDOM.render(
+      <chunk.Library2 />,
+      document.getElementById(`placeholder2`)
+    )
+  })
+}, 4000)
+
+const redStyle = () => css`
+  color: red;
+`
+
+const Title = styled.h1`
+  color: pink;
+  ${redStyle};
+`
+
+ReactDOM.render(<Title>Welcome!</Title>, document.getElementById(`app`))
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: false,
+    port: 1337
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react')
+    },
+  },
+  devtool: `source-map`
+};
